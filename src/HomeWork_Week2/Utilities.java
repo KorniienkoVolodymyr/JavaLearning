b34e8a3537bb8d0592bdde9ddfab233909d61312
volodymyrkorniienko <v.a.korniienko@gmail.com>
2017-03-06 01:34:42 +0200

    Utilitites

--- src/HomeWork_Week2/Utilities.java
+++ src/HomeWork_Week2/Utilities.java
@@ -17,4 +17,14 @@ public class Utilities {
         }
         return array;
     }
+
+    public static void maxValueInArray(int[] array) {
+        int maxValue = array[0];
+        for (int i = 1; i < array.length ; i++) {
+            if (array[i] > maxValue) {
+                maxValue = array[i];
+            }
+        }
+        System.out.println(maxValue);
+    }
 }
