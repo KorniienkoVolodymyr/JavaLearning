ad3e60caacb7c099ebba3db41631df7cae3fc1c4
volodymyrkorniienko <v.a.korniienko@gmail.com>
2017-03-07 00:11:15 +0200

    Week2 HomeWork Task 10

---
+++ src/HomeWork_Week2/Task10_2.java
@@ -0,0 +1,20 @@
+package HomeWork_Week2;
+
+import java.util.Scanner;
+
+/**
+ * Created by volodymyrkorniienko on 06.03.17.
+ */
+public class Task10_2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter some text: ");
+        String text = scanner.nextLine();
+
+        String[] words = text.split(" ");
+        char[] charArray = words[2].toCharArray();
+        System.out.println("First letter in third word is " + charArray[0]);
+    }
+}
